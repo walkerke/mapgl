8a26751aa1c1fe2c1020efbd9196eb72931bf6e1
Kyle Walker <kyle@walker-data.com>
2024-06-21 11:21:30 -0500

    update markers functionality

--- R/markers.R
+++ R/markers.R
@@ -1,10 +1,10 @@
 #' Add markers to a Mapbox GL or Maplibre GL map
 #'
 #' @param map A map object created by the `mapboxgl` or `maplibre` functions.
-#' @param data A length-2 numeric vector of coordinates or an `sf` POINT object.
-#' @param color The color of the marker.
-#' @param rotation The rotation of the marker.
-#' @param popup A column name for popups (if data is an `sf` object) or a string for a single popup (if data is a numeric vector).
+#' @param data A length-2 numeric vector of coordinates, a list of length-2 numeric vectors, or an `sf` POINT object.
+#' @param color The color of the marker (default is "red").
+#' @param rotation The rotation of the marker (default is 0).
+#' @param popup A column name for popups (if data is an `sf` object) or a string for a single popup (if data is a numeric vector or list of vectors).
 #' @param ... Additional options passed to the marker.
 #'
 #' @return The modified map object with the markers added.
@@ -12,18 +12,33 @@
 #'
 #' @examples
 #' \dontrun{
+#' library(mapgl)
+#' library(sf)
+#'
+#' # Create a map object
 #' map <- mapboxgl(
-#'   style = "mapbox://styles/mapbox/streets-v12",
+#'   style = mapbox_style("streets"),
 #'   center = c(-74.006, 40.7128),
-#'   zoom = 10,
-#'   access_token = "your_token_here"
+#'   zoom = 10
 #' )
 #'
 #' # Add a single marker
 #' map <- add_markers(map, c(-74.006, 40.7128), color = "blue", rotation = 45, popup = "A marker")
 #'
+#' # Add multiple markers from a list of coordinates
+#' coords_list <- list(c(-74.006, 40.7128), c(-73.935242, 40.730610))
+#' map <- add_markers(map, coords_list, color = "green", popup = "Multiple markers")
+#'
+#' # Create an sf POINT object
+#' points_sf <- st_as_sf(data.frame(
+#'   id = 1:2,
+#'   lon = c(-74.006, -73.935242),
+#'   lat = c(40.7128, 40.730610)
+#' ), coords = c("lon", "lat"), crs = 4326)
+#' points_sf$popup <- c("Point 1", "Point 2")
+#'
 #' # Add multiple markers from an sf object
-#' map <- add_markers(map, your_sf_object, color = "red", popup = "your_column_name")
+#' map <- add_markers(map, points_sf, color = "red", popup = "popup")
 #' }
 add_markers <- function(map, data, color = "red", rotation = 0, popup = NULL, ...) {
   options <- list(...)
@@ -50,14 +65,23 @@ add_markers <- function(map, data, color = "red", rotation = 0, popup = NULL, ..
       popup = popup,
       options = options
     ))
+  } else if (is.list(data) && all(sapply(data, function(x) is.numeric(x) && length(x) == 2))) {
+    markers <- lapply(data, function(coord) {
+      list(
+        lng = coord[1],
+        lat = coord[2],
+        color = color,
+        rotation = rotation,
+        popup = popup,
+        options = options
+      )
+    })
   } else {
-    stop("Data must be either a length-2 numeric vector or an sf POINT object.")
+    stop("Data must be either a length-2 numeric vector, a list of length-2 numeric vectors, or an sf POINT object.")
   }
 
   if (any(inherits(map, "mapboxgl_proxy"), inherits(map, "maplibre_proxy"))) {
-
     proxy_class <- if (inherits(map, "mapboxgl_proxy")) "mapboxgl-proxy" else "maplibre-proxy"
-
     map$session$sendCustomMessage(proxy_class, list(id = map$id, message = list(type = "add_markers", markers = markers)))
   } else {
     if (!is.null(map$x$markers)) {
