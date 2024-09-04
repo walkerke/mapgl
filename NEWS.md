ebcec5e4febcbb02239f7d9a1a3c102cc08f9089
Kyle Walker <kyle@walker-data.com>
2024-09-04 11:59:55 -0500

    update docs with news

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,11 @@
+# mapgl 0.1.3
+
+* Geocoding support for Mapbox and MapLibre maps added with `add_geocoder_control()`
+* Freehand draw support in the draw toolbar with `add_draw_control(freehand = TRUE)`
+* A "reset view" control available with `add_reset_control()`
+* Circle clustering is streamlined with the `cluster_options()` function, to be used with the `cluster_options` argument in `add_circle_layer()` and `add_symbol_layer()`
+* Various bug fixes and performance improvements.
+
 # mapgl 0.1.0
 
 * Initial release.
--- R/controls.R
+++ R/controls.R
@@ -471,6 +471,8 @@ add_geocoder_control <- function(map,
 #'
 #' @param map A map object created by the `mapboxgl` or `maplibre` functions.
 #' @param position The position of the control. Can be one of "top-left", "top-right", "bottom-left", or "bottom-right". Default is "top-right".
+#' @param animate Whether or not to animate the transition to the original map view; defaults to `TRUE`.  If `FALSE`, the view will "jump" to the original view with no transition.
+#' @param duration The length of the transition from the current view to the original view, specified in milliseconds.  This argument only works with `animate` is `TRUE`.
 #'
 #' @return The modified map object with the reset control added.
 #' @export
