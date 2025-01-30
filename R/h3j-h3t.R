a7940fa33010128dc754c55de1086760a2c3e48e
Carl Boettiger <cboettig@gmail.com>
2025-01-30 18:29:37 +0000

    document :pen:

--- R/h3j-h3t.R
+++ R/h3j-h3t.R
@@ -1,8 +1,35 @@
 
 # addH3JSource
-
-
+#' Add a hexagon source from the H3 geospatial indexing system.
+#' @references https://h3geo.org, https://github.com/INSPIDE/h3j-h3t
+#' @inheritParams add_sources
 #' @export
+#' @examplesIf interactive()
+#' url = "https://inspide.github.io/h3j-h3t/examples/h3j/sample.h3j"
+#' maplibre(center=c(-3.704, 40.417), zoom=15, pitch=30) |>
+#'   add_h3j_sources("h3j_testsource",
+#'                   url = url
+#'   )  |>
+#'   add_fill_extrusion_layer(
+#'     id = "h3j_testlayer",
+#'     source = "h3j_testsource",
+#'     fill_extrusion_color = interpolate(
+#'       column = "value",
+#'       values = c(0, 21.864),
+#'       stops = c("#430254", "#f83c70")
+#'     ),
+#'     fill_extrusion_height = list(
+#'       "interpolate",
+#'       list("linear"),
+#'       list("zoom"),
+#'       14,
+#'       0,
+#'       15.05,
+#'       list("*", 10, list("get", "value"))
+#'     ),
+#'     fill_extrusion_opacity = 0.7
+#'   )
+#'
 add_h3j_sources <- function(map, id, url) {
   h3j_sources <- list(
     id = id,
@@ -18,3 +45,5 @@ add_h3j_sources <- function(map, id, url) {
 
   return(map)
 }
+
+
